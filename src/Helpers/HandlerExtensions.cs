8dab7db3e45a39f8a90f83dea3832500121b0877
Nikita Tulenev <n1tulenev@gmail.com>
2022-05-27 21:19:53 +0300

    Add HandlerExtensions

---
+++ src/Helpers/HandlerExtensions.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+
+namespace Helpers
+{
+    public static class HandlerExtensions
+    {
+        public static Func<CancellationToken, Task> WithScopedService<TService>(
+        this Func<TService, CancellationToken, Task> func,
+        IServiceScopeFactory serviceScopeFactory) where TService : notnull =>
+        async token =>
+        {
+            using var serviceScope = serviceScopeFactory.CreateScope();
+
+            var service = serviceScope.ServiceProvider.GetRequiredService<TService>();
+
+            await func(service, token).ConfigureAwait(false);
+        };
+
+        public static Func<CancellationToken, Task> WithLoop(
+        this Func<CancellationToken, Task> func,
+        PeriodicTimer periodicTimer) =>
+        async token =>
+        {
+            while (!token.IsCancellationRequested)
+            {
+                if (await periodicTimer.WaitForNextTickAsync(token).ConfigureAwait(false))
+                {
+                    await func(token).ConfigureAwait(false);
+                }
+            }
+
+            token.ThrowIfCancellationRequested();
+        };
+
+        public static Func<CancellationToken, Task> WithLoop(this Func<CancellationToken, Task> func) =>
+        async token =>
+        {
+            while (!token.IsCancellationRequested)
+            {
+                await func(token).ConfigureAwait(false);
+            }
+
+            token.ThrowIfCancellationRequested();
+        };
+    }
+}
