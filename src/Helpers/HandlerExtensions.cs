615f1f3e629d2a3153c3c559f97c8032f22e0329
Nikita Tulenev <n1tulenev@gmail.com>
2022-05-28 08:32:43 +0300

    Add helpers args validation

--- src/Helpers/HandlerExtensions.cs
+++ src/Helpers/HandlerExtensions.cs
@@ -6,41 +6,55 @@ namespace Helpers
     {
         public static Func<CancellationToken, Task> WithScopedService<TService>(
         this Func<TService, CancellationToken, Task> func,
-        IServiceScopeFactory serviceScopeFactory) where TService : notnull =>
-        async token =>
+        IServiceScopeFactory serviceScopeFactory) where TService : notnull
         {
-            using var serviceScope = serviceScopeFactory.CreateScope();
+            ArgumentNullException.ThrowIfNull(func);
+            ArgumentNullException.ThrowIfNull(serviceScopeFactory);
 
-            var service = serviceScope.ServiceProvider.GetRequiredService<TService>();
+            return async token =>
+            {
+                using var serviceScope = serviceScopeFactory.CreateScope();
 
-            await func(service, token).ConfigureAwait(false);
-        };
+                var service = serviceScope.ServiceProvider.GetRequiredService<TService>();
+
+                await func(service, token).ConfigureAwait(false);
+            };
+        }
 
         public static Func<CancellationToken, Task> WithLoop(
         this Func<CancellationToken, Task> func,
-        PeriodicTimer periodicTimer) =>
-        async token =>
+        PeriodicTimer periodicTimer)
         {
-            while (!token.IsCancellationRequested)
+            ArgumentNullException.ThrowIfNull(func);
+            ArgumentNullException.ThrowIfNull(periodicTimer);
+
+            return async token =>
             {
-                if (await periodicTimer.WaitForNextTickAsync(token).ConfigureAwait(false))
+                while (!token.IsCancellationRequested)
+                {
+                    if (await periodicTimer.WaitForNextTickAsync(token).ConfigureAwait(false))
+                    {
+                        await func(token).ConfigureAwait(false);
+                    }
+                }
+
+                token.ThrowIfCancellationRequested();
+            };
+        }
+
+        public static Func<CancellationToken, Task> WithLoop(this Func<CancellationToken, Task> func)
+        {
+            ArgumentNullException.ThrowIfNull(func);
+
+            return async token =>
+            {
+                while (!token.IsCancellationRequested)
                 {
                     await func(token).ConfigureAwait(false);
                 }
-            }
 
-            token.ThrowIfCancellationRequested();
-        };
-
-        public static Func<CancellationToken, Task> WithLoop(this Func<CancellationToken, Task> func) =>
-        async token =>
-        {
-            while (!token.IsCancellationRequested)
-            {
-                await func(token).ConfigureAwait(false);
-            }
-
-            token.ThrowIfCancellationRequested();
-        };
+                token.ThrowIfCancellationRequested();
+            };
+        }
     }
 }
