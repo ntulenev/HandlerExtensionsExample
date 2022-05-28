7d74c8bc2cb0fc445a2eb5e5a3ecaf7a7e32995e
Nikita Tulenev <n1tulenev@gmail.com>
2022-05-28 08:41:11 +0300

    Add helpers xml doc

--- src/Helpers/HandlerExtensions.cs
+++ src/Helpers/HandlerExtensions.cs
@@ -2,8 +2,19 @@
 
 namespace Helpers
 {
+    /// <summary>
+    /// Delegate-based helpers methods that simplify routine operation with handlers in Background services.
+    /// </summary>
     public static class HandlerExtensions
     {
+        /// <summary>
+        /// Helpers runs <paramref name="func"/> with <typeparamref name="TService"/> from scope.
+        /// </summary>
+        /// <typeparam name="TService">Service type.</typeparam>
+        /// <param name="func">Target delegate.</param>
+        /// <param name="serviceScopeFactory">DI service factory.</param>
+        /// <returns>Decorated delegate with sevice from scope.</returns>
+        /// <exception cref="ArgumentNullException">If <paramref name="func"/> or <paramref name="serviceScopeFactory"/> is null.</exception>
         public static Func<CancellationToken, Task> WithScopedService<TService>(
         this Func<TService, CancellationToken, Task> func,
         IServiceScopeFactory serviceScopeFactory) where TService : notnull
@@ -21,6 +32,13 @@ namespace Helpers
             };
         }
 
+        /// <summary>
+        /// Runs target delegate <paramref name="func"/> in loop with specific timer.
+        /// </summary>
+        /// <param name="func">Target delegate.</param>
+        /// <param name="periodicTimer">Timer for the loop.</param>
+        /// <returns>Decorated delegate with timer loop.</returns>
+        /// <exception cref="ArgumentNullException">If <paramref name="func"/> or <paramref name="periodicTimer"/> is null.</exception>
         public static Func<CancellationToken, Task> WithLoop(
         this Func<CancellationToken, Task> func,
         PeriodicTimer periodicTimer)
@@ -42,6 +60,12 @@ namespace Helpers
             };
         }
 
+        /// <summary>
+        /// Runs target delegate <paramref name="func"/> in loop.
+        /// </summary>
+        /// <param name="func">Target delegate.</param>
+        /// <returns>Decorated delegate with loop.</returns>
+        /// <exception cref="ArgumentNullException">If <paramref name="func"/> is null</exception>
         public static Func<CancellationToken, Task> WithLoop(this Func<CancellationToken, Task> func)
         {
             ArgumentNullException.ThrowIfNull(func);
