b346939de6b3009c5df4991d3a3643d9e94c0a33
Nikita Tulenev <n1tulenev@gmail.com>
2023-02-19 20:16:42 +0100

    Add SimpleService xmldoc comments

--- src/ExampleApp/Services/SimpleService.cs
+++ src/ExampleApp/Services/SimpleService.cs
@@ -4,8 +4,19 @@ using Helpers;
 
 namespace ExampleApp.Services;
 
+/// <summary>
+/// Represents a simple background service that executes a default handler with a scoped service and a loop.
+/// </summary>
 public class SimpleService : BackgroundService
 {
+
+    /// <summary>
+    /// Initializes a new instance of the SimpleService class with the specified service scope factory and logger.
+    /// </summary>
+    /// <param name="serviceScopeFactory">The service scope factory.</param>
+    /// <param name="logger">The logger.</param>
+    /// <exception cref="ArgumentNullException">Thrown when serviceScopeFactory or logger is null.</exception>
+
     public SimpleService(IServiceScopeFactory serviceScopeFactory, ILogger<SimpleService> logger)
     {
         _serviceScopeFactory = serviceScopeFactory ?? throw new ArgumentNullException(nameof(serviceScopeFactory));
