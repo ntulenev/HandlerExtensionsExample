a64bb307f3f6fec281369c8850bfde4c46da6f77
Nikita Tulenev <n1tulenev@gmail.com>
2023-02-19 20:10:22 +0100

    Add IHandler xmldoc comments

--- src/ExampleApp/Handlers/IHandler.cs
+++ src/ExampleApp/Handlers/IHandler.cs
@@ -1,6 +1,15 @@
 ﻿namespace ExampleApp.Handlers;
 
+/// <summary>
+/// Defines the contract for implementing a handler.
+/// </summary>
 public interface IHandler
 {
+    /// <summary>
+    /// Handles the incoming data asynchronously.
+    /// </summary>
+    /// <param name="data">The data to be handled.</param>
+    /// <param name="cancellationToken">The cancellation token to cancel the operation.</param>
+    /// <returns>A task that represents the asynchronous operation.</returns>
     public Task HandleAsync(string data, CancellationToken cancellationToken);
 }
