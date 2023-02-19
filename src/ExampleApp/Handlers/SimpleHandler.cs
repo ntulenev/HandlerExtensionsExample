d86630663c9238b8d9ea77c0d60d56bc591f35c4
Nikita Tulenev <n1tulenev@gmail.com>
2023-02-19 20:12:50 +0100

    Add SimpleHandler xmldoc comments

--- src/ExampleApp/Handlers/SimpleHandler.cs
+++ src/ExampleApp/Handlers/SimpleHandler.cs
@@ -1,11 +1,26 @@
 ﻿namespace ExampleApp.Handlers;
 
+/// <summary>
+/// Implements the IHandler interface for handling data asynchronously.
+/// </summary>
 public class SimpleHandler : IHandler
 {
+    /// <summary>
+    /// Initializes a new instance of the SimpleHandler class.
+    /// </summary>
+    /// <param name="logger">The logger used to log events.</param>
+    /// <exception cref="ArgumentNullException">Thrown when logger is null.</exception>
     public SimpleHandler(ILogger<SimpleHandler> logger)
     {
         _logger = logger ?? throw new ArgumentNullException(nameof(logger));
     }
+
+    /// <summary>
+    /// Handles the given data asynchronously.
+    /// </summary>
+    /// <param name="data">The data to be handled.</param>
+    /// <param name="cancellationToken">The cancellation token.</param>
+    /// <returns>A task that represents the asynchronous operation.</returns>
 
     public async Task HandleAsync(string data, CancellationToken cancellationToken)
     {
