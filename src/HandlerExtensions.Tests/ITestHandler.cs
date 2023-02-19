ceb2506c869733f97e198788aac1edd9aca1a717
Nikita Tulenev <n1tulenev@gmail.com>
2023-02-19 20:08:12 +0100

    Add test handler interface

---
+++ src/HandlerExtensions.Tests/ITestHandler.cs
@@ -0,0 +1,15 @@
+﻿namespace HandlerExtensions.Tests;
+
+/// <summary>
+/// Defines a handler interface for processing test data.
+/// </summary>
+public interface ITestHandler
+{
+    /// <summary>
+    /// Processes the specified test data asynchronously.
+    /// </summary>
+    /// <param name="data">The test data to be processed.</param>
+    /// <param name="cancellationToken">A cancellation token that can be used to cancel the operation.</param>
+    /// <returns>A task representing the asynchronous operation.</returns>
+    public Task HandleAsync(string data, CancellationToken cancellationToken);
+}
