66997b15192180456a10ef85746b423d89c62cfb
Nikita Tulenev <n1tulenev@gmail.com>
2026-01-11 08:29:48 +0100

    Adds suppression for CA1515 in test interface
    Adds pragma directives to suppress CA1515 warning for the ITestHandler interface.
    This allows the interface to be used effectively with mocking frameworks in tests without generating warnings related to the usage of types derived from exceptions in public APIs.

--- src/HandlerExtensions.Tests/ITestHandler.cs
+++ src/HandlerExtensions.Tests/ITestHandler.cs
@@ -1,9 +1,11 @@
-﻿namespace HandlerExtensions.Tests;
+namespace HandlerExtensions.Tests;
 
 /// <summary>
 /// Defines a handler interface for processing test data.
 /// </summary>
+#pragma warning disable CA1515 // Need for Mocking in tests
 public interface ITestHandler
+#pragma warning restore CA1515 
 {
     /// <summary>
     /// Processes the specified test data asynchronously.
@@ -11,5 +13,5 @@ public interface ITestHandler
     /// <param name="data">The test data to be processed.</param>
     /// <param name="cancellationToken">A cancellation token that can be used to cancel the operation.</param>
     /// <returns>A task representing the asynchronous operation.</returns>
-    public Task HandleAsync(string data, CancellationToken cancellationToken);
+    Task HandleAsync(string data, CancellationToken cancellationToken);
 }
